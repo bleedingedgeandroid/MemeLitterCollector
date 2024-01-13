10fc747eb63a5cccd5e8c43bfbda3f13171d209e
PugzAreCute <me@pugzarecute.com>
2024-01-13 22:08:57 +0530

    Add download size calculator

--- collect.py
+++ collect.py
@@ -6,6 +6,7 @@ device = input("Codename: ")
 content = requests.get("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomifirmwareupdater.github.io/master/pages/miui/full/{}.md".format(device))
 
 images = re.findall("\"\/.*\/.*\/.*\/\"",content.text)
+dload_size = 0 #GB
 for i in images:
     os=i[:-2].split("/")[1]
     ver= i[:-2].split("/")[-1]
@@ -14,6 +15,19 @@ for i in images:
         print("Skipping {}, no FastBoot Download found.".format(ver))
         continue
     print("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomifirmwareupdater.github.io/master/pages/{}/updates/{}/{}.md".format(os,device,ver))
+    fb_size = re.findall(''' *<li style=\"padding-bottom: 10px;\">
+ *<h5><b>Type: </b>Fastboot</h5>
+ *</li>
+ *<li style=\"padding-bottom: 10px;\">
+ *<h5><b>Size: </b>\d*[.]\d GB</h5>
+ *</li>''',release.text)
+    if len(fb_size) != 1:
+        print("Something went wrong. Exiting.")
+        continue
+    actual_size = float(re.search("\d*[.]\d* GB",fb_size[0]).group()[:-3]) # remove " GB"
+    dload_size +=actual_size
+
 
 
 print("Collecting {} puppies".format(device))
+print("Xiaomi serious skill issue, left {} GB of puppies behind".format(dload_size))
