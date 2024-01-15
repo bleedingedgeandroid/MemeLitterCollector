747cbc35ef36d38100e2a1337f9094662ea7040d
PugzAreCute <me@pugzarecute.com>
2024-01-15 10:45:23 +0530

    Authorize to github and calculate files to be downloaded

--- collect.py
+++ collect.py
@@ -2,8 +2,18 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+from env import TOKEN
+from github import Github,Auth,UnknownObjectException
+
+
+
 #device = input("Codename: ")
 device="spes"
+user = "MIUI-MIRROR"
+auth = Auth.Token(TOKEN)
+hub = Github(auth=auth)
+repo = hub.get_repo("{}/{}".format(user,device))
+print(repo.url)
 content = requests.get("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomifirmwareupdater.github.io/master/pages/miui/full/{}.md".format(device))
 fb_rom_regex=''' *<li style=\"padding-bottom: 10px;\">
  *<h5><b>Type: </b>Fastboot</h5>
@@ -12,13 +22,18 @@ fb_rom_regex=''' *<li style=\"padding-bottom: 10px;\">
  *<h5><b>Size: </b>\d*[.]\d GB</h5>
  *</li>'''
 
+def sortInternal(list):
+    list.sort(key=lambda x: x[1])
+    return list
+
 images = set(re.findall("\"\/.*\/.*\/.*\/\"",content.text))
-dload_size = 0 #GB
+#dload_size = 0 #GB
+total_size = 0 #GB
 
 allowed_domains = "cdn-ota\.azureedge\.net|cdnorg\.d\.miui\.com|bn\.d\.miui\.com"
 valid_url_regex = "https://({})/.*\.tgz".format(allowed_domains)
-print(valid_url_regex)
 releases =[]
+
 for i in images:
     os=i[:-2].split("/")[1]
     ver= i[:-2].split("/")[-1]
@@ -32,7 +47,8 @@ for i in images:
         print("Something went wrong. Exiting.")
         continue
     actual_size = float(re.search("\d*[.]\d* GB",fb_size[0]).group()[:-3]) # remove " GB"
-    dload_size +=actual_size
+    print(actual_size)
+    total_size +=actual_size
     prettified_page = BeautifulSoup(requests.get("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomifirmwareupdater.github.io/master/pages/{}/updates/{}/{}.md".format(os,device,ver)).text).prettify()
     all_links = re.findall(" *<button class=\"btn btn-primary\" id=\"download\" onclick=\"window\.open\('https://.*\.tgz', '_blank'\);\" style=\"margin: 7px;\" type=\"button\">",prettified_page)
     links = []
@@ -41,10 +57,21 @@ for i in images:
         if f:
             links.append(f[0])
     print(links)
-    releases.append([os,ver,links])
-
-
+    releases.append([os,ver,links,actual_size])
 
 print("Collecting {} puppies".format(device))
-print("Xiaomi serious skill issue, left {} GB of puppies behind".format(dload_size))
+print("Xiaomi has {} GB of puppies".format(total_size))
 
+releases = sortInternal(releases)
+
+for i in releases:
+    print(i[3])
+    try:
+        repo.get_release(i[1]) # This will error out if the repo does not have this release.
+        print("Release found for {}".format(i[1]))
+        releases.remove(i)
+        total_size -= i[3]
+    except UnknownObjectException:
+        print("No release found for {}".format(i[1]))
+
+print("Need to download {} gigabytes of images".format(total_size))
